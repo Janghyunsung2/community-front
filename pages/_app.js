8cc662edaea3af843191afdad5f008201859e4ec
Janghyunsung2 <tlswlfk@gmail.com>
2025-03-04 18:58:58 +0900

    seo제거

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import { AuthProvider } from "../contexts/AuthContext";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout"; // default export 확인
-import SEOHead from "@/components/SEOHead";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
