e8de77dc40f6aa2317735c794233ab6bc1f855b2
Janghyunsung2 <tlswlfk@gmail.com>
2025-03-04 19:04:42 +0900

    seo제거

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,6 +1,5 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import api from "@/utils/axios";
-import SEOHead from "@/components/SEOHead";
 
 export const AuthContext = createContext();
 
