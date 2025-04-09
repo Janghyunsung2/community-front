332b15904a105e275e075cd6859b312e4d00e018
jsj <tmdwn3318@gmail.com>
2025-04-09 15:30:18 +0900

    로그인 성공 후 원래 있던 페이지로 돌아감

--- pages/login.js
+++ pages/login.js
@@ -25,7 +25,7 @@ export default function Login() {
             .then(() => {
                 console.log("로그인 성공");
                 setUser(true);
-                router.push("/");
+                router.back();
             })
             .catch((err) => {
                 console.error("로그인 실패:", err);
